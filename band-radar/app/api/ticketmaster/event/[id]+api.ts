7b89fd5ad314c4432ba5397ffb80117f571b45d8
frbarbre <fr.barbre@gmail.com>
2025-06-27 14:11:05 -0400

    added api endpoint for getting event details

--- band-radar/app/api/ticketmaster/event/[id]+api.ts
+++
@@ -1,6 +0,0 @@
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-  const id = new URL(request.url).searchParams.get('id');
-  console.log(id);
-
-  return Response.json({ id });
-}
